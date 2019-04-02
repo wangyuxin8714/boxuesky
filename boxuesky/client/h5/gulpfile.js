4518eb4e3992b9b2c4755c9f0ebae4ff9343dc41
Erynia <378572568@qq.com>
2019-04-02 10:34:00 +0800

    增加了个人书架页

--- boxuesky/client/h5/gulpfile.js
+++ boxuesky/client/h5/gulpfile.js
@@ -1,9 +1,21 @@
-var gulp = require("gulp");
-var webserver = require("gulp-webserver");
-var sass = require("gulp-sass");
+let [
+	gulp,
+	sass,
+	rename,
+	minCss,
+	autoprefixer,
+	webserver
+] = [
+	require("gulp"),
+	require("gulp-sass"),
+	require("gulp-rename"),
+	require("gulp-clean-css"),
+	require("gulp-autoprefixer"),
+	require("gulp-webserver")
+]
 
 gulp.task("sass",function(){
-	return gulp.src("./src/scss/*.scss")
+	return gulp.src("./src/scss/!(bookrack)*.scss")
  	.pipe(sass())
  	.pipe(gulp.dest("./src/css"))
  })
@@ -21,6 +33,20 @@ gulp.task("sass",function(){
 // 	}))
 // })
 
+gulp.task("devCss",function (){
+	return gulp.src("./src/scss/bookrack.scss")
+	.pipe(sass())
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions']
+	}))
+	.pipe(minCss())
+	.pipe(rename({
+		suffix: ".min"
+	}))
+	.pipe(gulp.dest("./src/css"))
+})
+
+
 gulp.task("webserver",function(){
 	gulp.src("./src")
 	.pipe(webserver({
@@ -30,12 +56,14 @@ gulp.task("webserver",function(){
 		proxies:[
 			{source:"/api/findFiction",target:"http://localhost:3000/api/findFiction"},
 			{source:"/api/findPart",target:"http://localhost:3000/api/findPart"},
+			{ source: "/api/findBookrack",target:"http://localhost:3000/api/findBookrack"}
 		]
 	}))
 })
 
 gulp.task("watch",function(){
-	 gulp.watch("./src/scss/*.scss",gulp.series("sass"))
+	gulp.watch(["./src/scss/bookrack.scss"],gulp.series(["devCss"]))
+	gulp.watch("./src/scss/*.scss",gulp.series("sass"))
 })
 
 gulp.task("dev",gulp.parallel("sass","webserver","watch"))
--- boxuesky/client/h5/src/js/inside.js
+++ boxuesky/client/h5/src/js/inside.js
@@ -1,10 +1,15 @@
 require.config({
+	baseUrl : "../js",
 	paths:{
-		"mui":"/libs/mui.min"
+		"mui":"libs/mui.min"
 	}
 })
 
-
 require(["mui"],function(mui){
-	
+	let bindEvent = function () {
+		mui(".main").on('tap','#bookrack',function () {
+			window.location.href = "../page/bookrack.html"
+		})
+	}
+	bindEvent()
 })
